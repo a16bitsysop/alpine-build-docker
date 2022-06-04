36b21397a53613327df67b56845a0c73ec8126e9
Duncan Bellamy <dunk@denkimushi.com>
2022-06-04 13:36:44 +0100

    remove moustache

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ die() {
   exit 1
 }
 
-[ -z "${ALPINE_PRIVATE_KEY}" ] && die "Please put private key in the ALPINE_PRIVATE_KEY environment variable"
+[ -z "$ALPINE_PRIVATE_KEY" ] && die "Please put private key in the ALPINE_PRIVATE_KEY environment variable"
 mkdir -p /home/"$NME"/.abuild
-echo "${ALPINE_PRIVATE_KEY" >> /home/"$NME"/.abuild/privkey.rsa
+echo "$ALPINE_PRIVATE_KEY" >> /home/"$NME"/.abuild/privkey.rsa
 echo "PACKAGER_PRIVKEY=\"/home/$NME/.abuild/privkey.rsa\"" > /home/"$NME"/.abuild/abuild.conf
 
 sudo apk -U upgrade -a
 
-mkdir -p /alpine/package
-cd /alpine/aport
-abuild -r -D "${REPO_DESC}" -P /alpine/package
+cd /alpine/aport || die "Please mount aport to build into /alpine/aport"
+echo "Building for repo $REPO_DESC"
+abuild -r -D "$REPO_DESC" -P /alpine/package
